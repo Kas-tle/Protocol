05ab0c250e9d2a9b42cf4765756d01b8d5b896db
Kas-tle <26531652+Kas-tle@users.noreply.github.com>
2025-12-10 14:19:29 -0800

    Invalid item runtime ids are only 0 and -1

--- bedrock-codec/src/main/java/org/cloudburstmc/protocol/bedrock/codec/v431/BedrockCodecHelper_v431.java
+++ bedrock-codec/src/main/java/org/cloudburstmc/protocol/bedrock/codec/v431/BedrockCodecHelper_v431.java
@@ -37,7 +37,7 @@ public class BedrockCodecHelper_v431 extends BedrockCodecHelper_v428 {
     @Override
     public ItemData readItemInstance(ByteBuf buffer) {
         int runtimeId = VarInts.readInt(buffer);
-        if (runtimeId == 0) {
+        if (runtimeId == 0 || runtimeId == -1) {
             // We don't need to read anything extra.
             return ItemData.AIR;
         }
@@ -106,7 +106,7 @@ public class BedrockCodecHelper_v431 extends BedrockCodecHelper_v428 {
     @Override
     public ItemData readItem(ByteBuf buffer) {
         int runtimeId = VarInts.readInt(buffer);
-        if (runtimeId <= 0) {
+        if (runtimeId == 0 || runtimeId == -1) {
             // We don't need to read anything extra.
             return ItemData.AIR;
         }
